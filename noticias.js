d999075d6e353bcb12a6a771c8497154c3964230
Seu Nome <seuemail@exemplo.com>
2025-04-25 12:44:20 -0300

    Melhora o design do site e adiciona notícias reais

--- noticias.js
+++ noticias.js
@@ -1,12 +1,27 @@
 const noticias = [
     {
-        "titulo": "Exemplo de Notícia 1",
-        "descricao": "Descrição da notícia 1.",
-        "link": "https://exemplo.com/noticia1"
+        "titulo": "GTA VI recebe data oficial de lançamento e novo trailer",
+        "descricao": "Rockstar Games anuncia que Grand Theft Auto VI chega em outubro de 2025 para PS5 e Xbox Series X|S, com versão para PC prevista para 2026.",
+        "link": "https://exemplo.com/gta6"
     },
     {
-        "titulo": "Exemplo de Notícia 2",
-        "descricao": "Descrição da notícia 2.",
-        "link": "https://exemplo.com/noticia2"
+        "titulo": "Nintendo revela novo console sucessor do Switch",
+        "descricao": "O esperado 'Switch 2' foi finalmente apresentado com gráficos em 4K e retrocompatibilidade com jogos do Switch original.",
+        "link": "https://exemplo.com/switch2"
+    },
+    {
+        "titulo": "Elden Ring: Shadow of the Erdtree recebe nota máxima em análises",
+        "descricao": "A expansão do jogo do ano de 2022 está sendo aclamada como uma das melhores DLCs de todos os tempos.",
+        "link": "https://exemplo.com/eldenring-dlc"
+    },
+    {
+        "titulo": "Microsoft anuncia aquisição de mais um estúdio de jogos",
+        "descricao": "Após Activision Blizzard, a gigante de tecnologia continua expandindo seu portfólio para o Xbox Game Pass.",
+        "link": "https://exemplo.com/microsoft-aquisicao"
+    },
+    {
+        "titulo": "Novo jogo da série God of War é anunciado para PS5",
+        "descricao": "Sony confirma que Kratos retornará em uma nova aventura, dando continuidade à saga nórdica iniciada em 2018.",
+        "link": "https://exemplo.com/god-of-war"
     }
 ];
